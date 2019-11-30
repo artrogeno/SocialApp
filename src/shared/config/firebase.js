56420b853923bc6d2d868ff160cc0dca072865ab
Arthur Costa da Silva <arthur.silva@pampa.compasso>
2019-11-30 18:27:15 -0300

    feat: Added config/firebase

---
+++ src/shared/config/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyAFfGMYnvYiqBeCZpwnz7U93GeVcDKD8Mc',
+  authDomain: 'socialapp-ccd4e.firebaseapp.com',
+  databaseURL: 'https://socialapp-ccd4e.firebaseio.com',
+  projectId: 'socialapp-ccd4e',
+  storageBucket: 'socialapp-ccd4e.appspot.com',
+  messagingSenderId: '988239201979',
+  appId: '1:988239201979:web:22366b6fc12744aab08b9e',
+};
+
+export default firebaseConfig;
